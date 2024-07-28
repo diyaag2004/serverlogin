f586d4935d57d94987f66629be69f1340a6ba117
Diya Agrawal <130734531+diyaag2004@users.noreply.github.com>
2024-07-28 14:50:45 +0530

    server side code for login/signup

---
+++ config.js
@@ -0,0 +1,6 @@
+export default {
+    JWT_SECRET : "HGVDRfas3456FFGB=KHJGY7T67=HVBTFYU",
+    EMAIL: "grayson.murazik33@ethereal.email", // testing email & password
+    PASSWORD : "aPn8g3qT3EnYaUbgG7",
+    ATLAS_URI: "mongodb+srv://admin:admin123@login-signup.bq9z2wx.mongodb.net/?retryWrites=true&w=majority&appName=login-signup"
+}
